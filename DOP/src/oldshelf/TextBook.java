5e937c49e6173c21f1d8196fb0a259730e37979d
mishu142 <meshaalshaik@gmail.com>
2024-11-04 01:30:26 +0530

    Update TextBook.java

--- DOP/src/oldshelf/TextBook.java
+++ DOP/src/oldshelf/TextBook.java
@@ -1,23 +1,9 @@
 package oldshelf;
 
-public class TextBook extends Book {
+public record TextBook(String title, String subject) implements Book {
 
-	// TODO: Make this a final field with most strict visiibility possible.
-	String subject;
-	
-	// TODO: Change the constructor if required? Correct any errors
-	public TextBook(String subject) {
-		super(subject);
-	}
-
-	// TODO : create a getter if required.
-	
-	// TODO: Create a setter if required
-	
-	// TODO: write a toString method
-	@Override
-	public String toString() {
-		return null;
-	}
-
+    @Override
+    public String toString() {
+        return "TextBook{title='" + title + "', subject='" + subject + "'}";
+    }
 }
