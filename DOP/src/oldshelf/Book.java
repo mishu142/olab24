66cdc662e799d38b8b5f32b4b70e39f3cff46cc4
mishu142 <meshaalshaik@gmail.com>
2024-11-04 01:27:26 +0530

    Update Book.java

--- DOP/src/oldshelf/Book.java
+++ DOP/src/oldshelf/Book.java
@@ -1,8 +1,6 @@
 package oldshelf;
 
-public abstract class Book {
-
-	public Book() {
-		// TODO: Finish Code here if anything to be done?
-	}
+public interface Book {
+    // Getter for title
+    String title();
 }
